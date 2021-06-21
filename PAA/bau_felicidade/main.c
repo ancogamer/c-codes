38ee1ab7b6b745e916d9c9a39390bb187108afca
João Vitor Astori Saletit <ancogamer@gmail.com>
2021-06-21 20:39:12 -0300

    update git ignore e atualizando c files

--- PAA/bau_felicidade/main.c
+++ PAA/bau_felicidade/main.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 int main(){	
-	
+	int N; // qtd vagas  
+	int somaPeso;
+
+
+	// sempre dividir por 2 pessoas.
+    scanf("%d ",&N); 
+	int pesos[N];
+	for (int i=0; i<N;i++){
+		scanf("%d ",&pesos[i]);
+		somaPeso+=pesos[i];
+	}
+	int mod=somaPeso%2;
+	printf("%d",mod);
+
 	return 0;
 
 } 
--- PAA/entre_nos/main.c
+++ PAA/entre_nos/main.c
@@ -7,3 +7,26 @@ int main(){
 	return 0;
 
 } 
+
+/* some notes
+
+M E N C 
+M = numero de salas
+E = ligações entre elas corredores
+N = ligações entre elas tubulações
+C = consultas
+
+U E V = inteiros 
+D = float
+
+
+M E N C 
+U V D
+U V D
+U V D
+
+
+
+4 linha -> consulta 
+
+*/
